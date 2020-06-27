8c8a9e922b5b2b17266971776f6b47650942b8ef
Admer <49600504+Admer456@users.noreply.github.com>
2020-06-27 16:02:23 +0200

    Remove last MISSIONPACK thingy

--- code/game/g_active.c
+++ code/game/g_active.c
@@ -173,12 +173,6 @@ G_SetClientSound
 ===============
 */
 void G_SetClientSound( gentity_t *ent ) {
-#ifdef MISSIONPACK
-	if( ent->s.eFlags & EF_TICKING ) {
-		ent->client->ps.loopSound = G_SoundIndex( "sound/weapons/proxmine/wstbtick.wav");
-	}
-	else
-#endif
 	if (ent->waterlevel && (ent->watertype&(CONTENTS_LAVA|CONTENTS_SLIME)) ) {
 		ent->client->ps.loopSound = level.snd_fry;
 	} else {
