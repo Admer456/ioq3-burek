60213615e77179e9238a556c12f2161fa104923b
Admer <49600504+Admer456@users.noreply.github.com>
2020-09-13 21:53:56 +0200

    Add BinaryCopier (optional copy tool)

---
+++ build/Copy.bat
@@ -0,0 +1,4 @@
+BinaryCopier.exe "GameExeDir" "ioquake3.x86" "BinExeDir_Debug"
+BinaryCopier.exe "GameDir"       "cgamex86"     "BinDir_Debug"
+BinaryCopier.exe "GameDir"       "qagamex86"   "BinDir_Debug"
+BinaryCopier.exe "GameDir"       "uix86"             "BinDir_Debug"
