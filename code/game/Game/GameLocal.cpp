5e0811477a5e197b2e72067b4b4be95a95772514
Admer <49600504+Admer456@users.noreply.github.com>
2020-11-15 20:35:40 +0100

    Game: Implement ClientConnect

--- code/game/Game/GameLocal.cpp
+++ code/game/Game/GameLocal.cpp
@@ -1,9 +1,14 @@
+#include "Maths/Vector.hpp"
 #include "Game/g_local.hpp"
 
 #include "Game/IGame.h"
 #include "Game/IGameImports.h"
 #include "Game/GameExportImport.h"
 #include "Game/GameLocal.h"
+
+#include "Entities/IEntity.hpp"
+#include "Entities/BaseEntity.hpp"
+#include "Entities/BasePlayer.hpp"
 
 #include "Game/GameWorld.hpp"
 
@@ -35,7 +40,101 @@ void GameLocal::Shutdown( bool restart )
 
 const char* GameLocal::ClientConnect( int clientNum, bool firstTime, bool isBot )
 {
-	return ::ClientConnect( clientNum, firstTime, isBot );
+	char* value;
+	//	char		*areabits;
+	gclient_t* client;
+	char userinfo[MAX_INFO_STRING];
+	Entities::BasePlayer* ent;
+
+	// if a player reconnects quickly after a disconnect, the client disconnect may never be called, thus flag can get lost in the ether
+	if ( gEntities[clientNum] )
+	{
+		G_LogPrintf( "Forcing disconnect on active client: %i\n", clientNum );
+		// so lets just fix up anything that should happen on a disconnect
+		ClientDisconnect( clientNum );
+	}
+
+	ent = gameWorld->CreateEntity<Entities::BasePlayer>( clientNum );
+
+	gameImports->GetUserInfo( clientNum, userinfo, sizeof( userinfo ) );
+
+	// IP filtering
+	// https://zerowing.idsoftware.com/bugzilla/show_bug.cgi?id=500
+	// recommanding PB based IP / GUID banning, the builtin system is pretty limited
+	// check to see if they are on the banned IP list
+	value = Info_ValueForKey( userinfo, "ip" );
+	
+	if ( G_FilterPacket( value ) ) 
+	{
+		return "You are banned from this server.";
+	}
+
+	// we don't check password for bots and local client
+	// NOTE: local client <-> "ip" "localhost"
+	//   this means this client is not running in our current process
+	if ( !isBot && (strcmp( value, "localhost" ) != 0) ) 
+	{
+		// check for a password
+		value = Info_ValueForKey( userinfo, "password" );
+		if ( g_password.string[0] && Q_stricmp( g_password.string, "none" ) &&
+			 strcmp( g_password.string, value ) != 0 ) 
+		{
+			return "Invalid password";
+		}
+	}
+	
+	// they can connect
+	ent->SetClient( &level.clients[clientNum] );
+	client = ent->GetClient();
+
+	memset( client, 0, sizeof( *client ) );
+
+	client->pers.connected = CON_CONNECTING;
+
+	// check for local client
+	value = Info_ValueForKey( userinfo, "ip" );
+	if ( !strcmp( value, "localhost" ) ) 
+	{
+		client->pers.localClient = qtrue;
+	}
+
+	// Admer: Bots aren't supported a.t.m.
+	/*if ( isBot ) 
+	{
+		ent->GetShared()->svFlags |= SVF_BOT;
+		if ( !G_BotConnect( clientNum, (qboolean)!firstTime ) ) 
+		{
+			return "BotConnectfailed";
+		}
+	}*/
+
+	// read or initialize the session data
+	if ( firstTime || level.newSession ) 
+	{
+		G_InitSessionData( client, userinfo );
+	}
+	
+	G_ReadSessionData( client );
+
+	// get and distribute relevant parameters
+	G_LogPrintf( "ClientConnect: %i\n", clientNum );
+	ClientUserinfoChanged( clientNum );
+
+	// don't do the "xxx connected" messages if they were caried over from previous level
+	if ( firstTime ) 
+	{
+		trap_SendServerCommand( -1, va( "print \"%s" S_COLOR_WHITE " connected\n\"", client->pers.netname ) );
+	}
+
+	if ( g_gametype.integer >= GT_TEAM && client->sess.sessionTeam != TEAM_SPECTATOR ) 
+	{
+		BroadcastTeamChange( client, -1 );
+	}
+
+	// count current clients and rank for scoreboard
+	CalculateRanks();
+
+	return NULL;
 }
 
 void GameLocal::ClientBegin( int clientNum )
