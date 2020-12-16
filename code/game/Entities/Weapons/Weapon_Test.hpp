b979beb7f25594fbff0c5ebb25b81b8f56f81cec
Admer <49600504+Admer456@users.noreply.github.com>
2020-12-17 00:08:05 +0100

    Weapons: serverside Weapon_Test draft

---
+++ code/game/Entities/Weapons/Weapon_Test.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Entities
+{
+	class Weapon_Test final : public BaseWeapon
+	{
+	public:
+		DeclareEntityClass();
+
+		WeaponInfo	GetWeaponInfo() override;
+		uint16_t	GetWeaponFlags() { return WFNone; }
+
+		void		PrimaryFire() override;
+	};
+}
