001c239f0b43214aa12490c370bc30e60f40b82b
Admer <49600504+Admer456@users.noreply.github.com>
2020-12-17 00:09:31 +0100

    Client Weapon_Test: update

--- code/cgame/Weapons/Weapon_Test.cpp
+++ code/cgame/Weapons/Weapon_Test.cpp
@@ -1,9 +1,25 @@
 #include "cg_local.hpp"
 #include "WeaponFactory.hpp"
 #include "BaseClientWeapon.hpp"
+#include "../shared/Weapons/WeaponIDs.hpp"
 
 #include "Weapon_Test.hpp"
 
 using namespace ClientEntities;
 
-DefineWeaponClass( Weapon_Test, 0 );
+DefineWeaponClass( Weapon_Test, WeaponID_Test );
+
+BaseClientWeapon::WeaponInfo ClientEntities::Weapon_Test::GetWeaponInfo()
+{
+	WeaponInfo wi;
+
+	wi.name = "Test weapon";
+	wi.viewModel = "models/weapons/v_test.iqm";
+
+	return wi;
+}
+
+void ClientEntities::Weapon_Test::OnPrimaryFire()
+{
+	Com_Printf( "Client weapon: Firing!\n" );
+}
--- code/cgame/Weapons/Weapon_Test.hpp
+++ code/cgame/Weapons/Weapon_Test.hpp
@@ -5,6 +5,8 @@ namespace ClientEntities
 	class Weapon_Test : public BaseClientWeapon
 	{
 	public:
+		WeaponInfo GetWeaponInfo() override;
 
+		void OnPrimaryFire() override;
 	};
 }
