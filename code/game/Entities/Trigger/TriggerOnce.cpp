4e6cc84bff822a2ced89a6350f2abf3886815ec8
Admer <49600504+Admer456@users.noreply.github.com>
2020-12-26 15:51:31 +0100

    Entities: implement TriggerOnce

---
+++ code/game/Entities/Trigger/TriggerOnce.cpp
@@ -0,0 +1,42 @@
+#include "Maths/Vector.hpp"
+
+#include "Game/g_local.hpp"
+#include "Entities/BaseEntity.hpp"
+#include "Entities/KeyValueElement.hpp"
+#include "Game/GameWorld.hpp"
+#include "../qcommon/IEngineExports.h"
+#include "Game/IGameImports.h"
+#include "Entities/Base/BaseTrigger.hpp"
+
+#include "TriggerOnce.hpp"
+
+using namespace Entities;
+
+DefineEntityClass( "trigger_once", TriggerOnce, BaseTrigger );
+
+void TriggerOnce::Spawn()
+{
+	BaseTrigger::Spawn();
+
+	GetShared()->contents |= CONTENTS_TRIGGER;
+
+	SetTouch( &TriggerOnce::OnceTouch );
+	SetUse( &TriggerOnce::OnceUse );
+}
+
+void TriggerOnce::OnceTouch( IEntity* other, trace_t* trace )
+{
+	engine->Print( "TriggerOnce::OnceTouch\n" );
+
+	OnceUse( other, other, 0 );
+}
+
+void TriggerOnce::OnceUse( IEntity* activator, IEntity* caller, float value )
+{
+	SetTouch( nullptr );
+	SetUse( nullptr );
+
+	UseTargets( activator );
+
+	Remove();
+}
---
+++ code/game/Entities/Trigger/TriggerOnce.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Entities
+{
+	class IEntity;
+	class BaseQuakeEntity;
+	class BaseTrigger;
+	
+	class TriggerOnce : public BaseTrigger
+	{
+	public:
+		DeclareEntityClass();
+
+		void Spawn() override;
+
+		void OnceTouch( IEntity* other, trace_t* trace );
+		void OnceUse( IEntity* activator, IEntity* caller, float value );
+	};
+}
