9996f4b672db2aba0b4c6fb2af8d07933ae98236
Admer <49600504+Admer456@users.noreply.github.com>
2021-01-02 13:23:21 +0100

    bg_misc: adjust extrapolation time to 16ms

--- code/shared/bg_misc.cpp
+++ code/shared/bg_misc.cpp
@@ -1235,7 +1235,7 @@ void BG_PlayerStateToEntityStateExtraPolate( playerState_t *ps, entityState_t *s
 	// set the time for linear prediction
 	s->pos.trTime = time;
 	// set maximum extra polation time
-	s->pos.trDuration = 50; // 1000 / sv_fps (default = 20)
+	s->pos.trDuration = 16; // 1000 / sv_fps (default = 60)
 
 	s->apos.trType = TR_INTERPOLATE;
 	VectorCopy( ps->viewangles, s->apos.trBase );
