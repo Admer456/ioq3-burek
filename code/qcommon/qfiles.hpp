fda90cb00ccaabad3d4fa880503f5d4f42eb9b92
Admer <49600504+Admer456@users.noreply.github.com>
2021-01-13 19:45:09 +0100

    Increase SHADER_MAX_VERTEXES for mega terrain models

--- code/qcommon/qfiles.hpp
+++ code/qcommon/qfiles.hpp
@@ -35,7 +35,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 // surface geometry should not exceed these limits
-#define	SHADER_MAX_VERTEXES	8000
+#define	SHADER_MAX_VERTEXES	16384
 #define	SHADER_MAX_INDEXES	(6*SHADER_MAX_VERTEXES)
 
 
