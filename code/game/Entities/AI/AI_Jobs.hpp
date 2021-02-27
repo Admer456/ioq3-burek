d757ef3e9f60d788114d4846dfb5445fadd48ec7
Admer <49600504+Admer456@users.noreply.github.com>
2021-02-27 22:51:48 +0100

    AI::Job draft

---
+++ code/game/Entities/AI/AI_Jobs.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace AI
+{
+	// A job is essentially a set of tasks for an NPC to do
+	// It's a purely logical thing, saying "commands" to the NPC
+	// such as, in this order:
+	// - go to this NPC
+	// - heal
+	// - return
+	// Jobs can be interrupted
+	class Job final
+	{
+	public:
+
+	};
+}
