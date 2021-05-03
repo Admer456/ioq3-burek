efdf3013604f8d19aae4f244ea5643b574b31e6c
Admer <49600504+Admer456@users.noreply.github.com>
2021-05-03 22:57:29 +0200

    Increase eventParm bits so we can send more stuff

--- code/qcommon/msg.cpp
+++ code/qcommon/msg.cpp
@@ -977,7 +977,7 @@ netField_t	entityStateFields[] =
 
 	// event stuff
 	netField_t{ NETF(event), 10 },
-	netField_t{ NETF(eventParm), 10 },
+	netField_t{ NETF(eventParm), 16 },
 	netField_t{ NETF(generic1), 10 },	// Sound ents can use generic1 to store a sound index,
 	netField_t{ NETF(eType), 10 },		// so I increased generic1 bits from 8 to 10, to have 1024 sounds -Admer
 	netField_t{ NETF(eFlags), 19 },
