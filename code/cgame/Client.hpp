8b9f5ab34182903fe3ea65b5bc15260dfbd605da
Admer <49600504+Admer456@users.noreply.github.com>
2021-05-08 21:24:06 +0200

    Client: more consistent tabs

--- code/cgame/Client.hpp
+++ code/cgame/Client.hpp
@@ -12,32 +12,32 @@ public:
 	~Client();
 
 	// Updates all systems for the given moment
-	void		Update();
+	void				Update();
 	// Called BEFORE every level change or subsystem restart
-	void		PreReload();
+	void				PreReload();
 	// Called AFTER every level change or subsystem restart
-	void		PostReload();
+	void				PostReload();
 	
 	// Before all HUD elements are drawn
-	void		PreDraw2D();
+	void				PreDraw2D();
 	// After all HUD elements have been drawn
-	void		PostDraw2D();
+	void				PostDraw2D();
 
 	// Retrieves the current weapon carried by the local player
 	ClientEntities::BaseClientWeapon* GetCurrentWeapon();
 	// Parses CE_ events
-	void		ParseComplexEvent( int id, centity_t* cent, Vector position );
+	void				ParseComplexEvent( int id, centity_t* cent, Vector position );
 	// Strips the extension off modelName, and parses the respective .mcfg file
-	void		RegisterModelConfigData( int id, const char* modelName );
+	void				RegisterModelConfigData( int id, const char* modelName );
 	
 	// Gets a list of model animation data for the given game model
 	std::vector<Assets::ModelAnimation>& GetAnimationsForModel( qhandle_t modelindex );
 	// Returns the current client time
-	float		Time() const;
+	float				Time() const;
 	// Gets the latest usercmd
-	usercmd_t	GetUsercmd() const;
+	usercmd_t			GetUsercmd() const;
 	// Is the client currently paused?
-	bool		IsPaused();
+	bool				IsPaused();
 	// Mouse coordinates
 	Vector				GetMouseXY();
 	void				SetMouseXY( int x, int y );
@@ -45,33 +45,33 @@ public:
 	// Subsystem getters
 
 	// Gets the local player's view
-	ClientView* GetView();
+	ClientView*			GetView();
 	// Gets the handler for CE_ events
 	ComplexEventHandler* GetEventHandler();
 	// Gets the vegetation system
-	VegetationSystem* GetVegetationSystem();
+	VegetationSystem*	GetVegetationSystem();
 	// Gets the light manager
-	LightManager* GetLightManager();
+	LightManager*		GetLightManager();
 	// Gets the particle manager
-	ParticleManager* GetParticleManager();
+	ParticleManager*	GetParticleManager();
 
 	// Is this entity the local client?
-	static bool IsLocalClient( centity_t* cent );
+	static bool			IsLocalClient( centity_t* cent );
 	// Executes a weapon event for a given predicted entity
-	static void ExecuteWeaponEvent( int id, centity_t* cent );
+	static void			ExecuteWeaponEvent( int id, centity_t* cent );
 
 	// Loads the map's .mus file, if it exists
-	void		InitDynamicMusic( const char* mapName );
+	void				InitDynamicMusic( const char* mapName );
 	// Fades out the current song and fades in the new one if necessary
 	// label = the left name in .mus entries
-	void		UpdateDynamicMusic( const char* label );
+	void				UpdateDynamicMusic( const char* label );
 
 private:
 	ComplexEventHandler* complexEventHandler{ nullptr };
-	ClientView* view{ nullptr };
-	VegetationSystem* vegetationSystem{ nullptr };
-	LightManager* lightManager{ nullptr };
-	ParticleManager* particleManager{ nullptr };
+	ClientView*			view{ nullptr };
+	VegetationSystem*	vegetationSystem{ nullptr };
+	LightManager*		lightManager{ nullptr };
+	ParticleManager*	particleManager{ nullptr };
 
 	Vector				mouseCoordinates{ Vector::Zero };
 
